57100f66beb1121a0631d55613b5f4eb1395efe5
Gustavo Bolivar <gusta_bolivar@hotmail.com>
2024-10-10 15:25:08 +0200

    mude todo el style de navbar a .css

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,22 +1,15 @@
-import { makeStyles } from '@mui/styles';
 import { Typography } from '@mui/material';
 import "./About.css";
 
 const About = ({title,id}) => {
-  const classes = useStyles();
+  
   return (
   <div className="Asection">
-    <div className={classes.Asectioncontent} id={id}>
+    <div className="Asectioncontent" id={id}>
       <Typography variant='h3'>{title}</Typography>
     </div>
   </div>
   )
 };
-
-const useStyles = makeStyles((theme) => ({
-  Asectioncontent: {
-
-  },
-}))
 
 export default About;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,7 +6,6 @@ import ContactPhoneTwoToneIcon from "@mui/icons-material/ContactPhoneTwoTone";
 import MenuIcon from '@mui/icons-material/Menu';
 import CancelIcon from '@mui/icons-material/Cancel';
 import { Divider, Drawer, IconButton, List, ListItem, ListItemIcon } from "@mui/material";
-import { makeStyles } from "@mui/styles";
 import "./Navbar.css";
 import { useState } from "react";
 import { Link } from "react-scroll";
@@ -14,29 +13,28 @@ import { Link } from "react-scroll";
 
 
 const Navbar = () => {
-  const classes = useStyles();
   const [open, setOpen] = useState(false)
 
   const links = [
     {
       id: "about",
       text: "About Me",
-      icon: <AccountCircleRoundedIcon fontSize="large" className={classes.me}/>,
+      icon: <AccountCircleRoundedIcon fontSize="large" className="me"/>,
     },
     {
       id: "skills",
       text: "Skills",
-      icon: <EmojiObjectsTwoToneIcon fontSize="large" className={classes.light}/>,
+      icon: <EmojiObjectsTwoToneIcon fontSize="large" className="light"/>,
     },
     {
       id: "proyects",
       text: "Proyects",
-      icon: <ConstructionTwoToneIcon fontSize="large" className={classes.key}/>,
+      icon: <ConstructionTwoToneIcon fontSize="large" className="key"/>,
     },
     {
       id: "contact",
       text: "Contact",
-      icon: <ContactPhoneTwoToneIcon fontSize="large" className={classes.contact}/>,
+      icon: <ContactPhoneTwoToneIcon fontSize="large" className="contact"/>,
     },
   ];
 
@@ -45,42 +43,40 @@ const Navbar = () => {
     <nav className="navbar">
       <div className="toolbar">
         <h1 className="tittle">Gustavo Bolivar</h1>
-        <List className={classes.menu}>
+        <List className="menu">
           {links.map(({ id, text }, index) => (
             <Link
               key={index}
               to={id}
               spy={true}
               smooth={true}
-              duration={500}
-              offset={-80}
+              duration={900}
+              offset={-90}
             >
               {/* offset nos da unos pixeles extra para que el titulo de cada link no quede debajo del navbar */}
               {text}
             </Link>
           ))}
         </List>
-        <IconButton edge="end" className={classes.menubutton} onClick={()=>setOpen(!open)}>
+        <IconButton edge="end" className="menubutton" onClick={()=>setOpen(!open)}>
           <MenuIcon fontSize="large" />
         </IconButton>
       </div>
     </nav>
       <Drawer anchor="right" open={open} onClose={()=> setOpen(false)}>
-        <IconButton>
-          <CancelIcon className={classes.cancelbutton} onClick={()=>setOpen(false)} fontSize="large"></CancelIcon>
+        <IconButton  onClick={()=>setOpen(false)} >
+          <CancelIcon className="cancelbutton" fontSize="large"></CancelIcon>
         </IconButton>
         <Divider/>
         {links.map(({ id, text, icon }, index) => (
             <Link
               key={index}
-              className={classes.sidebar}
-              to={id}
-              spy={true}
+              className="sidebar"
+              to={id}              spy={true}
               smooth={true}
-              duration={500}
-              offset={-80}
+              duration={900}
+              offset={-90}
               >
-              {/* offset nos da unos pixeles extra para que el titulo de cada link no quede debajo del navbar */}
               <ListItem component="h4">
                 <span>
                   <ListItemIcon>
@@ -96,60 +92,5 @@ const Navbar = () => {
   );
 };
 
-
-
-
-const useStyles = makeStyles((theme) => ({
-  menu: {
-    [theme.breakpoints.down("sm")]: {
-      display: "none",
-    },
-  },
-  menubutton: {
-    "&.MuiIconButton-root": {
-      display: "none",
-      [theme.breakpoints.down("sm")]: {
-        display: "block",
-        color: "white",
-        position: "absolute",
-        top:12,
-        right: 10,
-          "&:hover":{
-            color: "rgb(0,222,0)",
-          }
-      }
-    },
-  },
-  me: {
-    color: "#1d8bf0",
-  },
-  light: {
-    color: "#edff00",
-  },
-  key:{
-    color:"#b3b3b1",
-  },
-
-  contact:{
-    color:"#5fadf4",
-  },
-  sidebar:{
-    width: "50vw",
-    fontSize: "1.4rem",
-    fontWeight: "bold",
-    color: "#333",
-    margin: theme.spacing(10,0,0,4),
-    [theme.breakpoints.down("xs")]:{
-      width:"70vw",
-    },
-
-  },
-  cancelbutton:{
-    color: "rgba(255, 0, 0, 0.5)",
-    "&:hover": {
-      color: "rgba(255, 0, 0, 0.7)",
-  },
-},
-}));
 
 export default Navbar;
