65034f005ef40a26aa1262a8cae92ce40b254dde
Gustavo Bolivar <gusta_bolivar@hotmail.com>
2024-10-13 20:46:59 +0200

    montando proyects

--- src/components/Proyects/Proyects.jsx
+++ src/components/Proyects/Proyects.jsx
@@ -1,20 +1,55 @@
-import { Typography } from '@mui/material';
-import './Proyects.css'
+import { Card, CardContent, CardMedia, Grid, Link, Typography } from '@mui/material';
 import { makeStyles } from '@mui/styles';
+import mockData from '../../mockData'; 
+import './Proyects.css'
+
+
+
 const Proyects = ({title,id}) => {
   const classes = useStyles();
   return (
     <div className="Psection">
       <div className={classes.Psectioncontent} id={id}>
       <Typography variant='h3'>{title}</Typography>
+      <Grid container className={classes.grid}>
+        {
+          mockData.map(({title,image, link}, index)=>(
+            <Grid item key={index} xs={12} sm={6} md={4}>
+              <Card className={classes.card}>
+                <CardMedia image={image} className={classes.caratula} titulo="caratula"/>
+                <CardContent>
+                  <Link href={link} color="primary" target="_blank" rel="noopener noreferrer">
+                    {title}
+                  </Link>
+                </CardContent>
+              </Card>
+            </Grid>
+          ))
+        }
+
+      </Grid>
       </div>
     </div>
   )
 };
 
-const useStyles = makeStyles(() => ({
-  Asectioncontent: {
-
+const useStyles = makeStyles((theme) => ({
+  Psectioncontent:{
+    maxWidth:"90vw",
+    margin: "0 auto",
+    padding: theme.spacing(5),
+  },
+  grid: {
+    marginTop : theme.spacing(20),
+  },
+  card: {
+    maxWidth: 345,
+    minHeight: 275,
+    margin: theme.spacing(3),
+  },
+  caratula: {
+    height: 0,
+    paddingTop: '56.25%',
   },
 }))
 
---
+++ src/mockData.jsx
@@ -0,0 +1,23 @@
+import pokedex from '../src/assets/pokedex.jpeg';
+import rickMorty from '../src/assets/rick&morty.jpeg';
+import travelDiary from '../src/assets/travel-diary.jpeg';
+
+const mockData = [
+    {
+        title: "Pokedex de Pokémon",
+        image: pokedex,
+        link: "https://poke-evo-lution.vercel.app/"
+    },
+    {
+        title: "Rick and Morty",
+        image: rickMorty,
+        link: "https://rick-morty-nine-tau.vercel.app/"
+    },
+    {
+        title: "Travel Diary",
+        image: travelDiary,
+        link: "https://travel-diary-one.vercel.app/"
+    },
+];
+
+export default mockData;
